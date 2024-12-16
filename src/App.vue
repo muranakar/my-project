7242b652aa12a536d40cf0c1a1b080efffc1bab2
[muranakar] <[P1211065@gmail.com]>
2024-12-16 17:24:35 +0900

    一つの画面だけに変更

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,5 @@
 <template>
   <div id="app">
-    <nav v-if="showNav">
-      <router-link to="/"><button @click="navigate">Home</button></router-link>
-      <router-link to="/todo"><button @click="navigate">Todo List</button></router-link>
-      <router-link to="/sample1"><button @click="navigate">Sample Page 1</button></router-link>
-      <router-link to="/sample2"><button @click="navigate">Sample Page 2</button></router-link>
-      <router-link to="/sample3"><button @click="navigate">Page 3</button></router-link>
-      <router-link to="/sample4"><button @click="navigate">Page 4</button></router-link>
-    </nav>
     <router-view/>
   </div>
 </template>
@@ -17,13 +9,16 @@ export default {
   name: 'App',
   data() {
     return {
-      showNav: true
+      showNav: false
     };
   },
   methods: {
     navigate() {
       this.showNav = false;
     }
+  },
+  created() {
+    this.$router.push('/sample1');
   }
 }
 </script>
@@ -36,27 +31,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-nav {
-  margin-bottom: 20px;
-}
-button {
-  background-color: #2c3e50;
-  border: 1px solid #2c3e50;
-  border-radius: 5px;
-  color: white;
-  cursor: pointer;
-  padding: 10px 20px;
-  font: inherit;
-  transition: background-color 0.3s, color 0.3s;
-}
-
-button:hover {
-  background-color: white;
-  color: #2c3e50;
-}
-nav a {
-  margin-right: 10px;
-  text-decoration: none;
 }
 </style>
